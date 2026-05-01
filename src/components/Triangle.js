46b5f7e151e54a642e9a92a353da04cf0151cd0a
lizzaaabb <lbogveradze12@gmail.com>
2026-05-01 11:49:19 +0400

    crystal

--- src/components/Triangle.js
+++ src/components/Triangle.js
@@ -5,17 +5,19 @@ import '../styles/Triangle.css'
 function Triangle({ size = 100 }) {
   const px = typeof size === 'number' ? `${size}px` : size
   return (
-    <div className="pyramid-loader" style={{ width: px, height: px }}>
-      <div className="wrapper">
-        <span className="side side1"></span>
-        <span className="side side2"></span>
-        <span className="side side3"></span>
-        <span className="side side4"></span>
-        <span className="side side5"></span>
-        <span className="side side6"></span>
-        <span className="side side7"></span>
-        <span className="side side8"></span>
-        <span className="shadow"></span>
+    <div className="pyramid-wrapper" style={{ width: px, height: px }}>
+      <div className="pyramid-loader" style={{ width: px, height: px }}>
+        <div className="wrapper">
+          <span className="side side1"></span>
+          <span className="side side2"></span>
+          <span className="side side3"></span>
+          <span className="side side4"></span>
+          <span className="side side5"></span>
+          <span className="side side6"></span>
+          <span className="side side7"></span>
+          <span className="side side8"></span>
+          <span className="shadow"></span>
+        </div>
       </div>
     </div>
   )
