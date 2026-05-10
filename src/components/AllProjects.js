9ba6b17b70a5a90ad3ea6f72d7979fffe9ade7b8
lizzaaabb <lbogveradze12@gmail.com>
2026-05-10 12:00:48 +0400

    border box

---
+++ src/components/AllProjects.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import '../styles/AllProjects.css'
+
+function AllProjects() {
+  return (
+    <div className='all-projects'>
+      
+    </div>
+  )
+}
+
+export default AllProjects
